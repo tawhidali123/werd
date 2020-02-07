c1d8ba8798165002e7951da7c03861f53f56bcdc
Tawhid <gem@tawhid.lan1>
2020-02-06 21:31:28 -0500

    initial commit

---
+++ src/components/Choices.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Choices() {
+    return (
+        <div>
+            <ul>
+                <li>first option</li>
+                <li>second option</li>
+                <li>third option</li>
+                <li>answer option</li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/components/Question.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Question() {
+    return (
+        <div>
+            <h1>What is ... Question?</h1>
+        </div>
+    )
+}
